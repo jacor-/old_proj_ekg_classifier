ab10d5ad6dcf793ad8a5923d31bc82ef64ee4847
gemmed <gemmed@bd-vm-d-gemmed.fbd.local>
2014-06-20 14:58:20 +0200

    Settings added

---
+++ old_src/system/settings.py
@@ -0,0 +1,17 @@
+
+#ROOT_PATH = "C:\Users\jacordero\Desktop\Gem-Med Pruebas\Old Gem-Med Project\dades\\"
+ROOT_PATH = "/home/gemmed/gitRepos/gemmedData/dades/"
+FULL_SIGNAL_PATH = '/media/OS/holter_pfc/holters_signal/'
+FULL_DIAGNOSE_PATH = '/media/OS/holter_pfc/holters_complete_diagnose/'
+RESUMED_DIAGNOSES_PATH = ROOT_PATH + 'diagnoses/'
+RESUMED_SIGNAL_PATH = ROOT_PATH + 'signals/'
+MDB_PATH = ROOT_PATH + 'infoCasos/'
+INTERNAL_INFO = ROOT_PATH + 'infoCasos/info_casos_sistema/'
+HOST = "192.168.1.171"
+PORT = 27027
+REFERENCE_DIAGNOSER = 'gserra_03_05_2012'
+REDIAGNOSE_DIAGNOSER = 'gserra_03_05_2012'
+
+
+import holter_io as h_io
+
